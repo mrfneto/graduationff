a5d6a515451770bc2bbb2bd7497f9a4b74b1acca
mrfneto <mrfneto@gmail.com>
2025-08-27 19:54:26 -0300

    Update Logo

--- src/components/ui/AppLogo.vue
+++ src/components/ui/AppLogo.vue
@@ -6,11 +6,10 @@ defineProps({
 
 <template>
   <router-link :to="to" class="flex items-center space-x-2">
-    <!-- <img
+    <img
       src="@/assets/logo-farmc3a1cia.jpg"
       alt="Logo Faculdade de Farmácia - UFRJ"
       class="h-10 w-auto"
-    /> -->
-    <span class="font-bold test-lg">FF</span>
+    />
   </router-link>
 </template>
